ab2f6a206d44862b2dcb5831d89ff1fd8187cc18
Jerry <Jeremias.Boos@gmx.de>
2015-10-19 21:36:27 +0200

    added simple mehrhuhn logic and a background

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+
+	public struct Spawninfo{
+		float scaling = 1;
+		float points = 10;
+		float spawnChancePerSecond = 0.25f;
+
+	}
+
+	public Camera cam;
+	public float width;
+
+	public Spawninfo[] layer;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
