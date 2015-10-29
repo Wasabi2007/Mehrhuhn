44bc8c3752448402c5595c9e2dab7be2c6c8e158
Jerry <Jeremias.Boos@gmx.de>
2015-10-29 21:52:30 +0100

    Improved Spawning

--- Assets/Scripts/Mehrhuhn.cs
+++ Assets/Scripts/Mehrhuhn.cs
@@ -7,6 +7,11 @@ public class Mehrhuhn : MonoBehaviour {
 	public int points = 100;
 	public float speed = 1;
 	public bool goXPositiv = true;
+	public float targetHeight = 0;
+
+	public float maxX = 300;
+
+	public float lifeTime = -1;
 
 	private float xDirection = 1;
 
@@ -23,8 +28,23 @@ public class Mehrhuhn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float dt = Time.deltaTime;
+		xDirection = (goXPositiv ? 1 : -1);
 
 		rigid2d.position += Vector2.right * xDirection * speed*dt;
+		if (rigid2d.position.y < targetHeight && lifeTime > 0 ) {
+			rigid2d.position += Vector2.up * speed * dt;
+		}
+
+		if (lifeTime < 0) {
+			rigid2d.position -= Vector2.up * speed * dt;
+		}
+
+		if (rigid2d.position.x > maxX) {
+			MonoBehaviour.Destroy (this.gameObject);
+		}
+
+		lifeTime -= dt;
+
 	}
 
 	void OnMouseUp(){
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,12 +12,15 @@ public class Spawner : MonoBehaviour {
         public float spawnChancePerSecond;
         public Transform canvas;
         public int sortingOrder;
+		public Vector2 targetHight;
 	}
 
 	public Camera cam;
 	public Rect LevelSize;
 
 	public Spawninfo[] layer;
+
+	private float spawntick = 0;
 
 
 	// Use this for initialization
@@ -27,22 +30,53 @@ public class Spawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		spawntick -= Time.deltaTime;
+
+		if (spawntick > 0)
+			return;
+
+
         float spawnvalue = Random.value;
+		spawntick = 1;
 
         foreach (var spawn in layer)
         {
             float spawnvaluex = Random.value;
             float spawnvaluey = Random.value;
+			float direction = Random.value-0.5f; // <0  go Left spawn Right ; >0 go Rigth spawn Left
             if (spawnvalue <= spawn.spawnChancePerSecond)
             {
                 GameObject go = GameObject.Instantiate<GameObject>(baseHuhn.gameObject);
+				go.transform.SetParent(spawn.canvas);
+				go.transform.localScale *= spawn.scaling;
 
-                go.transform.SetParent(spawn.canvas);
-                go.transform.position = new Vector3(LevelSize.width * spawnvaluex + LevelSize.x + transform.position.x,
-                    LevelSize.height * spawnvaluey + LevelSize.y + transform.position.y, 0);
+				var targeth = (spawn.targetHight.y-spawn.targetHight.x)*spawnvaluey+spawn.targetHight.x+ transform.position.y;
+				var spriteRender = go.GetComponent<SpriteRenderer>();
+				spriteRender.sortingOrder = spawn.sortingOrder;
+				
+				
+				var mehrhuhn = go.GetComponent<Mehrhuhn>();
+				mehrhuhn.targetHeight = targeth;
 
-                var spriteRender = go.GetComponent<SpriteRenderer>();
-                spriteRender.sortingOrder = spawn.sortingOrder;
+				mehrhuhn.goXPositiv = direction > 0f;
+				mehrhuhn.points = spawn.points;
+				mehrhuhn.maxX = transform.position.x + LevelSize.width;
+
+
+				//out of screen spawn
+				if(Random.value >= 0.5){
+					if(direction < 0){
+						go.transform.position = new Vector3(LevelSize.width  + LevelSize.x + transform.position.x,targeth, 0);
+					} else{
+						go.transform.position = new Vector3(LevelSize.x + transform.position.x - 3 ,targeth, 0);
+					}					
+				} else{
+					go.transform.position = new Vector3(LevelSize.width*spawnvaluex + LevelSize.x + transform.position.x,LevelSize.y + transform.position.y, 0);
+				}
+
+				mehrhuhn.lifeTime = Random.Range(10,100);
+				go.SetActive(true);
+				
             }
         }
 	}
