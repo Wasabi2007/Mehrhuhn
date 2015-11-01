ca8c654c355e2eb7849bec0e17c5f7b6db5d0a94
Jerry <Jeremias.Boos@gmx.de>
2015-11-01 13:23:22 +0100

    added iTween for fancy animation

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Score : MonoBehaviour {
 
+	public Text Scorelabel;
+	public int score = 0;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		Scorelabel.text = string.Format("{0}",score);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void addScore(int toadd){
+		score += toadd;
+		Scorelabel.text = string.Format("{0}",score);
 	}
 }
