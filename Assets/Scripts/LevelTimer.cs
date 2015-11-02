d9932d7e440a900f5f043a12fc28f83025067d1d
Jerry <Jeremias.Boos@gmx.de>
2015-11-02 11:59:28 +0100

    implemented level Timer

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelTimer : MonoBehaviour {
+
+	public int Minutes = 2;
+	public int Seconds = 0;
+	public Text lable;
+
+	public int nextlevel = 2;
+
+	private float time_ = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		time_ = Minutes * 60 + Seconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time_ -= Time.deltaTime;
+		if (time_ <= 0) {
+			Application.LoadLevel(nextlevel);
+		}
+
+		lable.text = string.Format ("{0:00}:{1:00}", Mathf.FloorToInt(time_ / 60), time_ % 60);
+	}
+}
